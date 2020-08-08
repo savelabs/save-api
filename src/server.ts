11bee5b98a36b25612ae17ec3442701906faf41b
Iago <iagonobre22@gmail.com>
2020-08-08 00:45:07 -0300

    Ajuste Cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
@@ -12,6 +13,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
