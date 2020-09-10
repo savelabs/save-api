7936eb1295e1209d8569908f198a4eeb9327c516
Iago <iagonobre22@gmail.com>
2020-09-10 19:12:37 -0300

    Deploy

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 
   return response.status(500).json({
     status: 'error',
-    message: 'Internal server error',
+    message: 'Erro interno no servidor.',
   });
 });
 
